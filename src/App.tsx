dcdfe6f8a3f31ad4adfa4dad6fec0c9c01e37005
JustinRhodes14 <rhodesju21@gmail.com>
2026-07-28 00:48:42 -0400

    feat: remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import Footer from './components/Footer'
 
 export default function App() {
   return (
